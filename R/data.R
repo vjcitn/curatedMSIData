b09c61d189c6f150a126653f673c9c9c7b33d013
Vince Carey <stvjc@channing.harvard.edu>
2019-09-23 06:25:25 -0400

    first draft

---
+++ R/data.R
@@ -0,0 +1,29 @@
+#' representation of 3'UTR MSI events in TCGA from Cortes-Ciriano et al. 2017
+#' @docType data
+#' @import SummarizedExperiment
+#' @import GenomicRanges
+#' @format SummarizedExperiment
+#' @source \url{https://www.nature.com/articles/ncomms15180#Sec22}
+#' @note Supplementary data 6 from publication noted in Source.  See
+#' metadata() component of this SummarizedExperiment for more details.
+#' @examples
+#' molpo_3utr
+"molpo_3utr"
+#' representation of MSI events in coding regions TCGA from Cortes-Ciriano et al. 2017
+#' @docType data
+#' @format SummarizedExperiment
+#' @source \url{https://www.nature.com/articles/ncomms15180#Sec22}
+#' @note Supplementary data 5 from publication noted in Source. See
+#' metadata() component of this SummarizedExperiment for more details.
+#' @examples
+#' molpo_CDS
+"molpo_CDS"
+#' representation of 5'UTR MSI events in TCGA from Cortes-Ciriano et al. 2017
+#' @docType data
+#' @format SummarizedExperiment
+#' @source \url{https://www.nature.com/articles/ncomms15180#Sec22}
+#' @note Supplementary data 7 from publication noted in Source. See
+#' metadata() component of this SummarizedExperiment for more details.
+#' @examples
+#' molpo_5utr
+"molpo_5utr"
